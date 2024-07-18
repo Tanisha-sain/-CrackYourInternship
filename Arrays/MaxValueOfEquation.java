0f3e5d29f4bd3bc4e36c5d4d09d36f5b7052fb35
Tanisha-sain <131015973+Tanisha-sain@users.noreply.github.com>
2024-07-18 20:54:55 +0530

    MaxValueOfEquation

---
+++ Arrays/MaxValueOfEquation.java
@@ -0,0 +1,21 @@
+// Return the maximum value of the equation yi + yj + |xi - xj| where |xi - xj| <= k and 1 <= i < j <= points.length.
+// since i < j so, xi < xj
+// Hence the equation becomes yi + yj + (-(xi-xj)) = yi + yj + xj - xi
+
+
+class Solution {
+    public int findMaxValueOfEquation(int[][] points, int k) {
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> (a.getKey() == b.getKey() ? a.getValue() - b.getValue() : b.getKey() - a.getKey()));
+        int res = Integer.MIN_VALUE;
+        for (int[] point : points) {
+            while (!pq.isEmpty() && point[0] - pq.peek().getValue() > k) {
+                pq.poll();
+            }
+            if (!pq.isEmpty()) {
+                res = Math.max(res, pq.peek().getKey() + point[0] + point[1]);
+            }
+            pq.offer(new Pair<>(point[1] - point[0], point[0]));
+        }
+        return res;
+    }
+}
